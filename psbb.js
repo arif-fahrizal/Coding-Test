4a6924c861093dd67e65bf84900f4242a18d3299
Arif Fahrizal <112544136+arif-fahrizal@users.noreply.github.com>
2024-09-10 06:09:10 +0700

    commit

--- psbb.js
+++ psbb.js
@@ -4,14 +4,9 @@ const prompt = ps()
 let nFamilies = prompt('Input the number of families: ')
 let nMembers = prompt('Input the number of members in the family: ')
 
-console.log(nFamilies)
-console.log(nMembers)
+const members = nMembers.replace(/[ ]/g, '').split("").map(Number)
+const buses = Math.ceil(members.reduce((a, b) => a + b) / 4)
 
-const members = nMembers.replace(/[ ]/g, '').split("").map((a) => Number(a));
-const buses = Math.ceil(members.reduce((a, b) => a + b) / 4);
-
-if (members.length < nFamilies) {
-    console.log("Input must be equal with count of family");
-} else {
-    console.log(`Minimum bus required is: ${buses}`);
-}
+members.length < nFamilies
+  ? console.log("Input must be equal with count of family")
+  : console.log(`Minimum bus required is: ${buses}`)
--- sortchar.js
+++ sortchar.js
@@ -1,17 +1,15 @@
-let voc = [], con = []
+const obj = {}, voc = [], con = []
 
 const sortChar = text => {
-    const str = text.toLowerCase().replace(/[ ]/g, '').split(''), obj = {}
-
-    str.map(a => { 
-        for(let i of a){
-             obj[i] ? obj[i] += 1 : obj[i] = 1
-        }
-    })
-
-    for(let i in obj) {
-        i.match(/[aiueo]/g) ? voc.push(i.repeat(obj[i])) : con.push(i.repeat(obj[i]))
+  text.toLowerCase().replace(/[ ]/g, '').split('').map(a => {
+    for(let i of a){
+      obj[i] ? obj[i] += 1 : obj[i] = 1
     }
+  })
+  
+  for(let i in obj) {
+    i.match(/[aiueo]/g) ? voc.push(i.repeat(obj[i])) : con.push(i.repeat(obj[i]))
+  }
 }
 
 sortChar('Next Case')
